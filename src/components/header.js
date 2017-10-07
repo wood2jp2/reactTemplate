134dc92a18269585766c0f60165092d3750cee99
Josh Wood <wood2jp2@gmail.com>
2017-10-07 10:45:55 -0400

    react logo refactor

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Logo from './Logo'
 
 const Header = () => {
   return (
     <div>
     <header className="App-header">
       <h1 className="App-title">Welcome to React</h1>
+      <Logo />
     </header>
     <p className="App-intro">
       To get started, edit <code>src/App.js</code> and save to reload.
